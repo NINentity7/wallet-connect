3d8a647e43f772f951ec017244f3e493bb7c479d
CARLOS AUGUSTO <94069996+OnearthCarlos@users.noreply.github.com>
2022-04-27 21:24:23 -0300

    Chore: upload app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MoralisProvider } from 'react-moralis';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl="https://gfj6trn3i3wx.usemoralis.com:2053/server"
+      appId="Ko9jVsN1xgn5WM0PYUKbUVBXg5oY9AC6L521G6Sf"
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
